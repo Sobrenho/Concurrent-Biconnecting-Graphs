5df8148790df00b78682211b2e396afaf19caa2e
Arthur Sobrinho <arthur.sobrinho2014@gmail.com>
2024-11-28 10:41:03 -0300

    fix: typo em adjacencia e tipagem em teste

--- graphs/graphs.go
+++ graphs/graphs.go
@@ -5,6 +5,7 @@ type Edge struct {
 	V2 *Vertex
 }
 
+
 type Vertex struct {
 	Id		int
 	Color  	string
@@ -18,52 +19,53 @@ type Graph struct {
 	N 				int
 	M 				int
 	Vertices		[]Vertex
-	AdjancencyList 	map[*Vertex][]*Vertex
+	AdjacencyList 	map[*Vertex][]*Vertex
 }
 
 
 func (G *Graph) AmostrarVert() *Vertex{
-	for v := range G.AdjancencyList{
+	for v := range G.AdjacencyList{
 		return v
 	}
 	
-	return &Vertex{}
+	return nil
 }
 
 // Gera seguinte grafo: https://i.imgur.com/8s24EVp.png
 func GenGraph() (*Graph) {
 	G := new(Graph)
-	G.AdjancencyList = make(map[*Vertex][]*Vertex, 17)
+	G.AdjacencyList = make(map[*Vertex][]*Vertex, 17)
 
 	G.N = 18
 	G.M = 19
 	
-	var Vertices []*Vertex
+	G.Vertices = make([]Vertex, 0)
 
 	//Criação dos objetos vértice
 	for i := range G.N{
-		Vertices = append(Vertices, &Vertex{i, "WHITE", nil, 0,0, new(int)})
+		G.Vertices = append(G.Vertices, Vertex{i, "WHITE", nil,0,0, new(int)})
 	}
 	
 
 	//Criação das arestas
-	G.AdjancencyList[Vertices[0]] 	= []*Vertex{Vertices[1], Vertices[2]} 
-	G.AdjancencyList[Vertices[1]] 	= []*Vertex{Vertices[0], Vertices[3]}
-	G.AdjancencyList[Vertices[2]] 	= []*Vertex{Vertices[0], Vertices[3], Vertices[4]}
-	G.AdjancencyList[Vertices[3]] 	= []*Vertex{Vertices[1], Vertices[2], Vertices[4]}
-	G.AdjancencyList[Vertices[4]] 	= []*Vertex{Vertices[2], Vertices[3], Vertices[5]}
-	G.AdjancencyList[Vertices[5]] 	= []*Vertex{Vertices[4]}
-	G.AdjancencyList[Vertices[6]] 	= []*Vertex{Vertices[7], Vertices[8]}
-	G.AdjancencyList[Vertices[7]] 	= []*Vertex{Vertices[6], Vertices[8]}
-	G.AdjancencyList[Vertices[8]] 	= []*Vertex{Vertices[6], Vertices[7]}
-	G.AdjancencyList[Vertices[9]] 	= []*Vertex{Vertices[10], Vertices[11]}
-	G.AdjancencyList[Vertices[10]] 	= []*Vertex{Vertices[9], Vertices[11], Vertices[12]}
-	G.AdjancencyList[Vertices[11]] 	= []*Vertex{Vertices[9], Vertices[10], Vertices[12]}
-	G.AdjancencyList[Vertices[12]] 	= []*Vertex{Vertices[10], Vertices[11]}
-	G.AdjancencyList[Vertices[13]] 	= []*Vertex{Vertices[14], Vertices[16]}
-	G.AdjancencyList[Vertices[14]] 	= []*Vertex{Vertices[13], Vertices[15]}
-	G.AdjancencyList[Vertices[15]] 	= []*Vertex{Vertices[14], Vertices[16]}
-	G.AdjancencyList[Vertices[16]] 	= []*Vertex{Vertices[13], Vertices[15]}
+	G.AdjacencyList[&G.Vertices[0]] 	= []*Vertex{&G.Vertices[1],  &G.Vertices[2]} 
+	G.AdjacencyList[&G.Vertices[1]] 	= []*Vertex{&G.Vertices[0],  &G.Vertices[3]}
+	G.AdjacencyList[&G.Vertices[2]] 	= []*Vertex{&G.Vertices[0],  &G.Vertices[3], &G.Vertices[4]}
+	G.AdjacencyList[&G.Vertices[3]] 	= []*Vertex{&G.Vertices[1],  &G.Vertices[2], &G.Vertices[4]}
+	G.AdjacencyList[&G.Vertices[4]] 	= []*Vertex{&G.Vertices[2],  &G.Vertices[3], &G.Vertices[5]}
+	G.AdjacencyList[&G.Vertices[5]] 	= []*Vertex{&G.Vertices[4]}
+	G.AdjacencyList[&G.Vertices[6]] 	= []*Vertex{&G.Vertices[7],  &G.Vertices[8]}
+	G.AdjacencyList[&G.Vertices[7]] 	= []*Vertex{&G.Vertices[6],  &G.Vertices[8]}
+	G.AdjacencyList[&G.Vertices[8]] 	= []*Vertex{&G.Vertices[6],  &G.Vertices[7]}
+	G.AdjacencyList[&G.Vertices[9]] 	= []*Vertex{&G.Vertices[10], &G.Vertices[11]}
+	G.AdjacencyList[&G.Vertices[10]] 	= []*Vertex{&G.Vertices[9],  &G.Vertices[11], &G.Vertices[12]}
+	G.AdjacencyList[&G.Vertices[11]] 	= []*Vertex{&G.Vertices[9],  &G.Vertices[10], &G.Vertices[12]}
+	G.AdjacencyList[&G.Vertices[12]] 	= []*Vertex{&G.Vertices[10], &G.Vertices[11]}
+	G.AdjacencyList[&G.Vertices[13]] 	= []*Vertex{&G.Vertices[14], &G.Vertices[16]}
+	G.AdjacencyList[&G.Vertices[14]] 	= []*Vertex{&G.Vertices[13], &G.Vertices[15]}
+	G.AdjacencyList[&G.Vertices[15]] 	= []*Vertex{&G.Vertices[14], &G.Vertices[16]}
+	G.AdjacencyList[&G.Vertices[16]] 	= []*Vertex{&G.Vertices[13], &G.Vertices[15]}
+	G.AdjacencyList[&G.Vertices[17]] 	= nil
 
 	return G
 
@@ -73,26 +75,26 @@ func GenGraph() (*Graph) {
 
 func TesteBiconnect() (*Graph){
 	G := new(Graph)
-	G.AdjancencyList = make(map[*Vertex][]*Vertex, 6)
+	G.AdjacencyList = make(map[*Vertex][]*Vertex, 6)
 
 	G.N = 6
 	G.M = 7
 	
-	var Vertices []*Vertex
+	var Vertices []Vertex
 
 	//Criação dos objetos vértice
 	for i := range G.N{
-		Vertices = append(Vertices, &Vertex{i, "WHITE", nil, 0,0, new(int)})
+		Vertices = append(Vertices, Vertex{i, "WHITE", nil,0,0, new(int)})
 	}
 	
 
 	//Criação das arestas
-	G.AdjancencyList[Vertices[0]] 	= []*Vertex{Vertices[1], Vertices[2]} 
-	G.AdjancencyList[Vertices[1]] 	= []*Vertex{Vertices[0], Vertices[3]}
-	G.AdjancencyList[Vertices[2]] 	= []*Vertex{Vertices[0], Vertices[3], Vertices[4]}
-	G.AdjancencyList[Vertices[3]] 	= []*Vertex{Vertices[1], Vertices[2], Vertices[4]}
-	G.AdjancencyList[Vertices[4]] 	= []*Vertex{Vertices[2], Vertices[3] , Vertices[5]}
-	G.AdjancencyList[Vertices[5]] 	= []*Vertex{Vertices[4]}
+	G.AdjacencyList[&Vertices[0]] 	= []*Vertex{&Vertices[1], &Vertices[2]} 
+	G.AdjacencyList[&Vertices[1]] 	= []*Vertex{&Vertices[0], &Vertices[3]}
+	G.AdjacencyList[&Vertices[2]] 	= []*Vertex{&Vertices[0], &Vertices[3], &Vertices[4]}
+	G.AdjacencyList[&Vertices[3]] 	= []*Vertex{&Vertices[1], &Vertices[2], &Vertices[4]}
+	G.AdjacencyList[&Vertices[4]] 	= []*Vertex{&Vertices[2], &Vertices[3], &Vertices[5]}
+	G.AdjacencyList[&Vertices[5]] 	= []*Vertex{&Vertices[4]}
 
 	return G
 }
