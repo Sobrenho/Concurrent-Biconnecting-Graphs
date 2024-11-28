125ad500ae9a316b0c4b4cc0d62c8eeaa0153ab6
Arthur Sobrinho <arthur.sobrinho2014@gmail.com>
2024-11-28 10:50:01 -0300

    feat: DFS retorna componentes conexas

--- graphs/sequential.go
+++ graphs/sequential.go
@@ -1,39 +1,92 @@
 package graphs
 
-import "fmt"
+import (
+	"slices"
+)
 
-func (G *Graph) DFS_Sequencial() {
+
+
+// ----- DFS ----------------
+
+func (G *Graph) DFS_Sequencial() []*Graph{
+	connectedComponents := make([]*Graph, 0, G.N)
+
+
+
+	for v := range G.AdjacencyList{
+		v.Color = "WHITE"
+		v.Parent = nil
+	}
+
+	//Construir Lista de Adjacencia
+
 
 	time := 0
-	for u := range G.AdjancencyList {
-		u.Color = "WHITE"
-		u.Parent = nil
-	}
+	
+	for u := range G.AdjacencyList {
+		
+		if (*u).Color == "WHITE" {
 
-	for u := range G.AdjancencyList {
-		if u.Color == "WHITE" {
-			G.dfs_Visit(u, &time)
+			treeVertices := G.dfs_Visit(u, &time)
+			componentAdjacencyList := make(map[*Vertex][]*Vertex)
+
+			edges := 0 
+
+			for _, v := range treeVertices{
+
+				for _, w := range G.AdjacencyList[v]{
+					if slices.Contains(treeVertices, w){
+						componentAdjacencyList[v] = append(componentAdjacencyList[v], w)
+						edges++
+					}
+				}
+			}
+
+			//Transformar []*Vertex em []Vertex
+			new_graph_vertices := make([]Vertex, 0, len(treeVertices))
+			for _, ptr := range treeVertices{
+				new_graph_vertices = append(new_graph_vertices, *ptr)
+			}
+
+			connectedComponents = append(connectedComponents, &Graph{len(treeVertices), edges/2 , new_graph_vertices, componentAdjacencyList})
+
 		}
 	}
-
+	return connectedComponents
 }
 
-func (G *Graph) dfs_Visit(u *Vertex, time *int) { //DFS_Visit
+
+//Função Recursiva DFS 
+//Retorna uma lista com todos os vértices visitados abaixo do vértice u
+func (G *Graph) dfs_Visit(u *Vertex, time *int) []*Vertex{ 
 	*time += 1
 	u.Desc = *time
 	u.Color = "GRAY"
 
-	for _, v := range G.AdjancencyList[u] {
+	visitedVertices := make([]*Vertex, 0, G.N)
+	
+	for i := range G.AdjacencyList[u] {
+		v := G.AdjacencyList[u][i]
 		if v.Color == "WHITE" {
 			v.Parent = u
-			G.dfs_Visit(v, time)
+
+			visitedVertices = append(visitedVertices, G.dfs_Visit(v, time)...)
+
 		}
 	}
 		u.Color = "BLACK"
 		*time += 1
 		u.Fim = *time
+
+		return append(visitedVertices, u)
 }
 
+// -----------------------------------------
+
+
+
+
+// ------ Tarjan Componente Biconexa -------------
 
 func (G * Graph) DetectBiconnectedComponents() bool{
 	tempo := 0
