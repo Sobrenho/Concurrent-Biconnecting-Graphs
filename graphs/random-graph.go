af29c0fb8c17b93cebad881e816cb2e3c75a8f43
Erick Gaiote <erickgaiote_santos@hotmail.com>
2024-11-28 23:13:30 -0300

    feat: random-graph

---
+++ graphs/random-graph.go
@@ -0,0 +1,30 @@
+package graphs
+
+import "math/rand/v2"
+
+func NewRandomGraph(verticesCount int, edgesCount int) *GraphX {
+
+	graph := NewGraphX(verticesCount)
+
+	edgeExists := make(map[int]map[int]bool, verticesCount)
+	for u := 0; u < verticesCount; u++ {
+		edgeExists[u] = make(map[int]bool, verticesCount)
+	}
+
+	for graph.EdgesCount() < edgesCount {
+
+		u := rand.IntN(verticesCount)
+		v := rand.IntN(verticesCount)
+
+		if edgeExists[u][v] {
+			continue
+		}
+
+		graph.AddEdge(u, v)
+		edgeExists[u][v] = true
+		edgeExists[v][u] = true
+	}
+
+	return graph
+
+}
