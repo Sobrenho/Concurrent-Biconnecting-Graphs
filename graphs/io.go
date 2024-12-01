4e7dd301b04c42256cd69609109b65e6539ae83a
Erick Gaiote <erickgaiote_santos@hotmail.com>
2024-12-01 14:01:57 -0300

    feat: funções para escrever e ler grafos em arquivos

---
+++ graphs/io.go
@@ -0,0 +1,80 @@
+package graphs
+
+import (
+	"encoding/binary"
+	"os"
+)
+
+func (graph *GraphX) WriteTo(file *os.File) error {
+
+	err := binary.Write(file, binary.BigEndian, int64(graph.VerticesCount()))
+	if err != nil {
+		return err
+	}
+
+	for u := 0; u < graph.VerticesCount(); u++ {
+		
+		err = binary.Write(file, binary.BigEndian, int64(len(graph.Adjacents(u))))
+		if err != nil {
+			return err
+		}
+
+		for _, v := range graph.Adjacents(u) {
+
+			err = binary.Write(file, binary.BigEndian, int64(v))
+			if err != nil {
+				return err
+			}
+
+		}
+
+	}
+
+	return nil
+}
+
+func ReadGraphFrom(file *os.File) (*GraphX, error) {
+	
+	var verticesCount int64
+
+	err := binary.Read(file, binary.BigEndian, &verticesCount)
+	if err != nil {
+		return nil, err
+	}
+
+	graph := NewGraphX(int(verticesCount))
+	
+	sumOfDegrees := 0
+
+	for u := 0; u < graph.verticesCount; u++ {
+
+		var adjacentsSize int64
+
+		err = binary.Read(file, binary.BigEndian, &adjacentsSize)
+		if err != nil {
+			return nil, err
+		}
+
+		graph.adjacents[u] = make([]int, int(adjacentsSize))
+
+		sumOfDegrees += len(graph.adjacents[u])
+
+		for i := range graph.adjacents[u] {
+
+			var v int64
+
+			err = binary.Read(file, binary.BigEndian, &v)
+			if err != nil {
+				return nil, err
+			}
+
+			graph.adjacents[u][i] = int(v)
+
+		}
+
+	}
+
+	graph.edgesCount = sumOfDegrees / 2
+
+	return graph, nil
+}
