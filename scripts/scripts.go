ddb4237e5e58bd492b5795da0f8db8a8699b026e
Erick Gaiote <erickgaiote_santos@hotmail.com>
2024-12-01 14:02:17 -0300

    feat: scripts.go, MakeRandomGraphFile

---
+++ scripts/scripts.go
@@ -0,0 +1,38 @@
+package scripts
+
+import (
+	"log"
+	"os"
+	"strconv"
+	"trabfinal/graphs"
+)
+
+func MakeRandomGraphFile(args []string) {
+	
+	file, err := os.Create(args[2])
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	defer file.Close()
+
+	verticesCount, err := strconv.Atoi(args[0])
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	edgesCount, err := strconv.Atoi(args[1])
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	graph := graphs.NewRandomGraph(verticesCount, edgesCount)
+	err = graph.WriteTo(file)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+}
