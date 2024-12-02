aa4155b2faa61fb81b7d02bce7897fb63f3ce0a9
Arthur Sobrinho <arthur.sobrinho2014@gmail.com>
2024-12-02 18:57:33 -0300

    feat: adiciona cores e log pra medição

--- tests/time_exec.go
+++ tests/time_exec.go
@@ -40,6 +40,9 @@ func createCSV(timestamps [][]int64){
 
 
 func MeasureRunningTime(args []string){
+	//Constantes para logging
+	colorReset := "\033[0m"
+	colorGreen := "\033[32m"
 
 	iterationsPerGraphSizes, err := strconv.Atoi(args[0])
 	if err != nil {
@@ -61,12 +64,19 @@ func MeasureRunningTime(args []string){
 
 	for i, g_size := range graph_sizes{
 
+		fmt.Printf("Gerando grafo: %s...", g_size)
 		scripts.MakeRandomGraph(append(strings.Split(g_size, " "), "g"))
-	
+		fmt.Println(colorGreen, "done", colorReset)
+
+		fmt.Print("Sequencial...")
 		timeTable[i][0] = mean(iterationsPerGraphSizes, []string{"g", "out"}, scripts.RunDFSTarjan)
+		fmt.Println(colorGreen, "done", colorReset)
+
 
 		for j, numThreads := range []int{1,2,4,8,16}{
+			fmt.Printf("Concorrente - %d thread....", numThreads)
 			timeTable[i][j+1] = mean(iterationsPerGraphSizes, []string{"g", strconv.Itoa(numThreads), "out"}, scripts.RunSplatoonTarjan)
+			fmt.Println(colorGreen, "done", colorReset)
 		}
 	}
 
